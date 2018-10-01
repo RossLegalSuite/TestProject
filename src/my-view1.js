efc75f0669a78a33310f0df30daef1e9e6206650
RossLegalSuite <ross@legalsuite.co.za>
2018-10-01 12:45:45 +0200

    Grid Select Break

--- src/my-view1.js
+++ src/my-view1.js
@@ -22,8 +22,10 @@ class MyView1 extends PolymerElement {
 
 <div class="card">
 
-  <my-dialog></my-dialog>
-  <vaadin-grid aria-label="Remote Data Example" data-provider="[[dataProvider]]" size="[[size]]">
+
+    <vaadin-button theme="success primary" on-click="_testPageLoad">Test Page Load</vaadin-button>
+  
+  <vaadin-grid id="grid" aria-label="Remote Data Example" data-provider="[[dataProvider]]" size="[[size]]">
 
     <vaadin-grid-column width="60px" flex-grow="0">
       <template class="header">#</template>
@@ -54,7 +56,28 @@ class MyView1 extends PolymerElement {
 
     };
   }
+  _testPageLoad() {
+    //Set grid object.    
+    let grid = this.$.grid;
+    //Scroll to next page fires the dataprovider to laod the Page.
+    grid._scrollToIndex(75);
 
+//---
+    //Beat happens here, even thought grid._cache.items has 100 items in it, when creating the results to map to an array it only has 50 on the first click
+//--
+    console.log("grid._cache.items",grid._cache.items);
+    //Creates an Object of all the Items in the grid.
+    var result = Object.keys(grid._cache.items).map(k => grid._cache.items[k]);
+    console.log("result",result);
+    //Converts to an array.
+    let thisArray = Object.values(result);
+    //Searches array for an item
+    let thisItem = thisArray.find(item => item.firstName === "Faith");
+    //Hightlights the Item.
+    grid.selectItem(thisItem);
+
+
+}  
   ready() {
     super.ready();
     this.size = 200;
@@ -64,6 +87,7 @@ class MyView1 extends PolymerElement {
         callback(JSON.parse(xhr.responseText).result);
       };
       var index = params.page * params.pageSize;
+      console.log("Current Page Index: "+index);
       xhr.open('GET', 'https://demo.vaadin.com/demo-data/1.0/people?index=' + index + '&count=' + params.pageSize, true);
       xhr.send();
     };
